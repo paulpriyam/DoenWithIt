5859f6a3fce088dc702cd2718fd06c1184bfe5ae
paulpriyam <ppaul2204@gmail.com>
2023-07-15 11:48:56 +0530

    changed app icon and added images.

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, Button } from 'react-native';
 
 export default function App() {
+  console.log("App executed")
+
+  const buttonClicked=()=>{
+    alert("button clicked")
+  }
+ 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+      style={styles.imageStyle}
+      source={
+        require("./assets/icon.png")
+      }
+       />
+       <Image  
+       style={styles.imageStyle}
+       source={{
+        uri:"https://picsum.photos/200/300",
+        }}/>
+        <Button 
+        style={styles.buttonStyle}
+        title='click me' 
+        onPress={buttonClicked} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageStyle:{
+    height:200,
+    width:300
+  },
+  buttonStyle:{
+    backgroundColor:'red'
+  }
 });
